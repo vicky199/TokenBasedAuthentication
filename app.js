9a19421fe976660a82dd866ca673159523014dfc
vickyplease <bind.vikas1995@gmail.com>
2019-01-30 06:54:49 +0530

    logout added

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express=require('express');
 const bodyParser=require('body-parser');
 var login = require('./Login');
 var user = require('./User');
+let {tokenList}=require('./constants/constant')
 //end 
 var app = express();
 app.use(bodyParser.json());
@@ -11,6 +12,10 @@ app.use(bodyParser.json());
 app.use('/login',login)
 app.use(require('./Model/tokenChecker'))
 app.use('/User',user)
+app.get('/logout',(req,res)=>{
+tokenList.pop(req.currentUserToken);
+return res.status(200).send({message:"Logout successfully!" ,Result:"Logout successfully!",statusCode:200});
+})
 app.listen(3000,()=>{
     console.log('App running on 3000')
 })
